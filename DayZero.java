91b88621cfe6260256e6a2de93af319d4224eba3
Devanand Gowda <devanandgowda8@gmail.com>
2019-02-27 21:21:47 +0530

    Add the solution to day zero --The infamous first ever program in any language

---
+++ DayZero.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class DayZero {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); 
+		String inputString = scan.nextLine(); 
+		scan.close(); 
+		System.out.println("Hello, World.");
+        System.out.print(inputString);
+	}
+}
+
